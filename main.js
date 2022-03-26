da7bfa131e9e45973b9dc338a8dd969fed4c8c1f
Phoenixfusion <79008006+whitatkkrshreash@users.noreply.github.com>
2022-03-26 16:45:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status= "";
 video= "";
+objects=[];
 function preload(){
     video= createVideo('video.mp4');
     video.hide();
@@ -21,4 +22,24 @@ function modelLoaded(){
 }
 function draw(){
     image(video,0,0,480,380);
+    if (status!=""){
+        object_detector.detect(video,gotResult);
+        for(i=0;i<objects.length;i++){
+            document.getElementById('status').innerHTML="Status: Object Detected";
+            document.getElementById('number_objects').innerHTML="Number of objects detected are"+objects.length;
+            fill('#FF0000');
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+            noFill();
+            stroke('#FF0000');
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
 }
